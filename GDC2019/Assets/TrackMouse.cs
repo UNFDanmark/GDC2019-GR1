fe8be489ca3e86b19d659bd91b8947202f019975
Sound <teknisk@game.unf.dk>
2019-07-28 23:02:14 +0200

    TextMeshPro Goodbye for now my old pal. Plus background noises and random office supplies sounds gets played

--- GDC2019/Assets/TrackMouse.cs
+++ GDC2019/Assets/TrackMouse.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class TrackMouse : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Range(0,1920)]public int x;
+    [Range(0, 1080)]public int y;
+    [Range(0, 500)] public float Distance;
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log(1 - Vector2.Distance(Input.mousePosition, new Vector2(x, y)) / Distance);
+        if (Vector2.Distance(Input.mousePosition, new Vector2(x, y)) < Distance)
+           GetComponent<AudioSource>().volume = 1 - Vector2.Distance(Input.mousePosition, new Vector2(x, y)) / Distance;
     }
 }
