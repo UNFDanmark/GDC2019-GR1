abc9b30ba9963318dda8cfaf9a8f94f7859fa402
Prog <teknisk@game.unf.dk>
2019-07-28 23:01:02 +0200

    attempted fix for hatNum comparison

--- GDC2019/Assets/Scripts/GameManagerScript.cs
+++ GDC2019/Assets/Scripts/GameManagerScript.cs
@@ -8,19 +8,33 @@ public class GameManagerScript : MonoBehaviour
     public GameObject Man;
     public bool accept;
     public int score = 0;
+    public GameObject headObject;
+    public AssetListScript AssetScript;
+    public List<GameObject> Hats = new List<GameObject>();
+    public int hatNum2, hatNum1;
     // Start is called before the first frame update
     void Start()
     {
+        
         UpdateScoreText();
+        AssetScript = headObject.GetComponent<AssetListScript>();
+        hatNum1 = headObject.GetComponent<AssetListScript>().hatNum;
+        hatNum2 = Random.Range(0, Hats.Count);
     }
 
     // Update is called once per frame
     void Update()
     {
         Success();
-        
+        AssetListScript AssetScript = headObject.GetComponent<AssetListScript>();
+        AssetScript.hatNum = hatNum1;
+        if(hatNum2 == hatNum1)
+        {
+            print("True");
+        }
         
     }
+
     public void Success()
     {
         if (Input.GetKeyDown(KeyCode.A) && accept == true || Input.GetKeyDown(KeyCode.D) && accept == false && GameObject.FindGameObjectWithTag("Man") != null)
