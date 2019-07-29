19d33e510540deaa1e0050a1378af4ef1aab7af8
Prog <teknisk@game.unf.dk>
2019-07-29 20:20:38 +0200

    Til Nikolaj. Fra Jacob

--- GDC2019/Assets/Scripts/TimerCountdownScript.cs
+++ GDC2019/Assets/Scripts/TimerCountdownScript.cs
@@ -16,9 +16,10 @@ public class TimerCountdownScript : MonoBehaviour
     void Update()
     {
         time = time - Time.deltaTime;
+        if (time < 0) time = 0;
         float sec = time % 60;
-        GetComponent<Text>().text = "Time Left: " + ((time - sec) / 60) + (((int)sec < 10) ? ":0" : ":") + (int)sec;
-
+        GetComponent<Text>().text = "Time Left: " + (((time-sec)<60) ? "0" : ((time - sec) / 60).ToString()) + (((int)sec < 10) ? ":0" : ":") + (int)sec;
+        
         if (time <= 0) print("Game Over");
     }
 }
