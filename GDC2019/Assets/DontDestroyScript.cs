1ffe7d851a284782c37b8aa94802b69b9117e662
Prog <teknisk@game.unf.dk>
2019-07-29 23:54:13 +0200

    switched to new model + added paper list + game over screen

---
+++ GDC2019/Assets/DontDestroyScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyScript : MonoBehaviour
+{
+
+    public static GameObject Instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!Instance)
+            Instance = gameObject;
+        else Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
