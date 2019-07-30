8b128e6135ef6137d9dd3690ae3b60dd9f4dc1b3
Prog <teknisk@game.unf.dk>
2019-07-30 18:02:32 +0200

    fixed spawn for all acessories

--- GDC2019/Assets/Scripts/AssetListScript.cs
+++ GDC2019/Assets/Scripts/AssetListScript.cs
@@ -12,10 +12,10 @@ public class AssetListScript : MonoBehaviour
     public List<GameObject> eye = new List<GameObject>();
     //list of placements
     List<Vector3> hPlace = new List<Vector3> {new Vector3(0, 12.58f, 0), new Vector3(0, 13.9f, 0), new Vector3(0, 8.19f, 2)};
-    public int hatNum;
+    public int hatNum, chestNum, faceNum, eyeNum;
     public GameManagerScript ManagerScript;
     public GameObject GameManager;
-    GameObject Hat, Hat2, Chest, Eye, FaceHair;
+    GameObject Hat, Chest, Eye, FaceHair;
     
     public Vector3 ManPos = new Vector3();
     void Start()
@@ -24,10 +24,16 @@ public class AssetListScript : MonoBehaviour
         ManagerScript = GameManager.GetComponent<GameManagerScript>();
 
         hatNum = Random.Range(0, hats1.Count);
+        chestNum = Random.Range(0, chest.Count);
+        faceNum = Random.Range(0, faceHair.Count);
+        eyeNum = Random.Range(0, eye.Count);
 
         //spawns object
-        GameObject spawnObject = Instantiate(hats1[hatNum], transform);
+        GameObject spawnObjectH = Instantiate(hats1[hatNum], transform);
                 print(hatNum);
+        GameObject spawnObjectC = Instantiate(chest[chestNum], transform);
+        GameObject spawnObjectF = Instantiate(faceHair[faceNum], transform);
+        GameObject spawnObjectE = Instantiate(eye[eyeNum], transform);
     }
 
     // Update is called once per frame
