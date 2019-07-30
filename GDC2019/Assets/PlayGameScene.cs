ad94e47ab60fc0b623aeaf79a55071fb25a5d5c0
Sound <teknisk@game.unf.dk>
2019-07-30 13:24:49 +0200

    Main Menu added

---
+++ GDC2019/Assets/PlayGameScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayGameScene : MonoBehaviour
+{
+    public void BeginGame()
+    {
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+    }
+}
